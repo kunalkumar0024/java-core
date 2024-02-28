5956995b9886d99c2c7428d5f1d942eda5341e01
kunalkumar0024 <kunalkumar0024@gmail.com>
2024-02-28 21:31:14 +0530

    Added arrays basic program

---
+++ src/Arrays/ArraysDemo.java
@@ -0,0 +1,9 @@
+package Arrays;
+
+public class ArraysDemo {
+    public static void main(String[] args) {
+        String[] arr = {"Kuanl", "Manish", "Abhi"};
+        System.out.println("Length of array is : " + arr.length);
+        System.out.println("Elements of array are : " + arr[0] + ", " + arr[1] + ", " + arr[2]);
+    }
+}
